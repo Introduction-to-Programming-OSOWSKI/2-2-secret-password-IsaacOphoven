3e036ad390a8da06363932211b14c784f63fff94
isaacophoven <824067@mystma.org>
2021-09-28 09:10:48 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #WRITE YOUR CODE IN THIS FILE
+#ok
+
+def password(p):
+    if p == "Knights19":
+        return "ACCESS GRANTED"
+    else:
+        return "ACCESS DENIED"
+
+print(password("Knights19"))
